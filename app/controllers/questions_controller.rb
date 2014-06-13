705624550c2b020d4200efca6894266d0ef18869
RSid <sm1th303@gmail.com>
2014-06-13 14:46:56 -0400

    Can create and view new questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -7,4 +7,24 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
   end
 
+  def create
+    @question = Question.new(question_params)
+
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @question =  Question.new()
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :question, :user_id)
+  end
+
 end
